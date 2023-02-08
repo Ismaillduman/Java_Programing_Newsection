62e3d73c07a8b87b047efc60f55f1f7f142181bc
issmail <issmailduman@gmail.com>
2023-02-08 13:40:14 +0100

    First Commit

---
+++ src/day_01/GasReceipt.java
@@ -0,0 +1,16 @@
+package day_01;
+
+import java.sql.SQLOutput;
+
+public class GasReceipt {
+
+    public static void main(String[] args) {
+        System.out.println("            Mclean Store            ");
+        System.out.println("");
+        System.out.println("04-29-2022           02:45 pm");
+        System.out.println("Gallons:       11:48");
+        System.out.println("Price/Gallon:   $4.15");
+        System.out.println("");
+        System.out.println("FUEL Total:     $47.642");
+    }
+}
---
+++ src/day_01/HelloWorld.java
@@ -0,0 +1,11 @@
+package day_01;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("How are you");
+    }
+}
