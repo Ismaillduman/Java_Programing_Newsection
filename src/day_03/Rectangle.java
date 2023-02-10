1617324c71d91bdf6e7f6dc6bc90c3843ac2702f
issmail <issmailduman@gmail.com>
2023-02-10 12:13:19 +0100

    rectangel

---
+++ src/day_03/Rectangle.java
@@ -0,0 +1,15 @@
+package day_03;
+
+public class Rectangle {
+    public static void main(String[] args) {
+
+        double width= 2.5;
+        double length= 3.4;
+        double area;
+
+        area = width * length;
+
+        System.out.println("area = " + area);
+
+    }
+}
