5a1bec4113ee0f3517371a254b0c13595b46ed2f
issma <issmailduman@gmail.com>
2023-02-18 12:49:37 +0100

    Revert last commit
    This reverts commit 8f288b89d67fbae12e75a4cef89dffc6a8b7fd77.

--- src/day_06/CharacterIdentity.java
+++ src/day_06/CharacterIdentity.java
@@ -3,15 +3,15 @@ package day_06;
 public class CharacterIdentity {
     public static void main(String[] args) {
 
-   char ch= '!';
-       boolean isDigit=  ch<='9'&& ch>='0';
+   char ch= '@';
+       boolean isNumber=  ch<='9'&& ch>='0';
        boolean isAlphabetic= ch<='Z' && 'A'<=ch || ch<='z' && 'a'<=ch ;
        //boolean isSpecialChar= !isAlphabetic && !isNumber;
        String result = "";
 
        if(isAlphabetic){
            result= "Alphabetic";
-       } else if (isDigit) {
+       } else if (isNumber) {
            result= "Digit";
        }else{
            result="Special Character";
