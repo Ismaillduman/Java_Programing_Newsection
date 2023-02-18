8f288b89d67fbae12e75a4cef89dffc6a8b7fd77
issma <issmailduman@gmail.com>
2023-02-18 12:44:32 +0100

    understand Readify

--- src/day_06/CharacterIdentity.java
+++ src/day_06/CharacterIdentity.java
@@ -3,15 +3,15 @@ package day_06;
 public class CharacterIdentity {
     public static void main(String[] args) {
 
-   char ch= '@';
-       boolean isNumber=  ch<='9'&& ch>='0';
+   char ch= '!';
+       boolean isDigit=  ch<='9'&& ch>='0';
        boolean isAlphabetic= ch<='Z' && 'A'<=ch || ch<='z' && 'a'<=ch ;
        //boolean isSpecialChar= !isAlphabetic && !isNumber;
        String result = "";
 
        if(isAlphabetic){
            result= "Alphabetic";
-       } else if (isNumber) {
+       } else if (isDigit) {
            result= "Digit";
        }else{
            result="Special Character";
