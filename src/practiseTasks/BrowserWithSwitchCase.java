e89b667323632ef8302f2ef21ff4cae88034cc61
Ismail Duman <96315214+Issmailduman@users.noreply.github.com>
2023-02-24 18:41:44 +0100

    Zubeyde (#33)
    * calculator with ternary
    
    * calculator with switch case
    
    * status code
    
    * browser selection
    
    ---------
    
    Co-authored-by: busraDuman <ismaidumann@web.de>

---
+++ src/practiseTasks/BrowserWithSwitchCase.java
@@ -0,0 +1,29 @@
+package practiseTasks;
+
+public class BrowserWithSwitchCase {
+    public static void main(String[] args) {
+
+        /* write a program  the selected browser
+            3.1  declare a String variable called browserName
+            3.2  Assume that the valid browsers are: chrome, firefox, opera, safari, edge
+            3.3 if the browser name does not match with the valid browsers' names, out put should be: Invalid Browser
+
+        	Do Not use if statement or ternary*/
+
+String browserName="";
+switch (browserName){
+    case "chrome":
+        break;
+    case "firefox":
+        break;
+    case "opera":
+        break;
+    case "safari":
+        break;
+    case "edge":
+        break;
+    default:
+        System.out.println("Invalid Browser");
+}
+    }
+}
