74cc1037d6272e4ac3b561e6ea64a98f0f1a55a8
busraDuman <ismaidumann@web.de>
2023-02-24 12:17:00 +0100

    ternary

---
+++ src/practiseTasks/NumberToWord.java
@@ -0,0 +1,27 @@
+package practiseTasks;
+
+public class NumberToWord {
+    public static void main(String[] args) {
+        /* Create a class called NumberToWord,
+		write a java program that can convert numbers between 0 ~ 9 to words
+			Ex:
+				number = 1;
+
+			output:
+				One
+
+		Note: MUST use ternary
+*/
+
+        int num = 10;
+        String result;
+
+        result = (num == 0) ? "zero" : (num == 1) ? "one" : (num == 2) ? "two" : (num == 3) ? "three" : (num == 4) ? "vier" :
+                (num == 5) ? "fünf" : (num == 6) ? "sechs" : (num == 7) ? "sieben" : (num == 8) ? "acht" : (num == 9) ? "neun" : "enter valid number";
+
+
+        System.out.println(result);
+
+
+    }
+}
