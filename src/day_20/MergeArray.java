ae5d0451b65c533d729e4c0f8755b446e71023bb
Ismailduman <issmailduman@gmail.com>
2023-03-24 13:58:52 +0100

    day_ 20 tasks

---
+++ src/day_20/MergeArray.java
@@ -0,0 +1,26 @@
+package day_20;
+
+import utilities.ArraysUtility;
+
+import java.util.Arrays;
+
+public class MergeArray {
+    public static void main(String[] args) {
+        int[] a={ 10,56,48,79,42};
+        int[] b={1,2,3,4,56,7};
+       int [] c= ArraysUtility.merge(a, b);
+        System.out.println(Arrays.toString(c));
+
+        double[] d={ 1.2,5.6,4.8,7.9,4.2};
+        double[] e={1,2,3,4,56,7};
+        double [] f=ArraysUtility.merge(d,e);
+        System.out.println(Arrays.toString(f));
+
+        String[] name= {"ismail","duman","busra"};
+        String[] name2= {"yusuf","duman","zubeyde"};
+        String[] merge=ArraysUtility.merge(name2,name);
+        System.out.println(Arrays.toString(merge));
+
+
+    }
+}
--- src/replit_tasks/week_7/ArrayMerge.java
+++ src/replit_tasks/week_7/ArrayMerge.java
@@ -9,7 +9,7 @@ class Main {
     public static int[] merge(int[] a,int[] b) {
 
 
-        int[] result = new int[a.length + b.length];
+       int[] result = new int[a.length + b.length];
 
 
         for (int i = 0; i < a.length; i++) {
