3a374c3df22ead22d2a862ae9c1004b7689ce90d
Ismailduman <issmailduman@gmail.com>
2023-03-25 14:09:38 +0100

    Shortest word

--- src/replit_tasks/week_7/ShortestWord.java
+++ src/replit_tasks/week_7/ShortestWord.java
@@ -4,6 +4,24 @@ import java.util.Arrays;
 
 public class ShortestWord {
     public static void main(String[] args) {
+        String[] str = {"olive", "fish", "pursuit", "old", "warning", "python", "java", "coffee", "cat", "ray"};
+
+        int minLength = Integer.MAX_VALUE;
+
+        for (String s : str) {
+            if (s.length() < minLength) {
+                minLength = s.length();
+
+            }
+
+        }
+
+
+        for (String s : str) {
+            if (s.length() == minLength) {
+                System.out.println(s);
+            }
+        }
 
     }
 
