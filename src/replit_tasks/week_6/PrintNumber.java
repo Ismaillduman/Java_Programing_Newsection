dcf845b8f7e79f20954a98738806b6ac14d4a38d
Ismailduman <issmailduman@gmail.com>
2023-03-28 13:34:14 +0200

    anagram and remove space

--- src/replit_tasks/week_6/PrintNumber.java
+++
@@ -1,9 +0,0 @@
-package replit_tasks.week_6;
-
-public class PrintNumber {
-    public static void main(String[] args) {
-        for (int i = 0; i <=50 ; i++) {
-            System.out.println(i);
-        }
-    }
-}
