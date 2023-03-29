114d3caa616692ca62e347cb4db1ad0c0c026852
Ismailduman <issmailduman@gmail.com>
2023-03-29 22:14:28 +0200

    wrapper class intro

--- src/day_22_arrayList/WrapperClassIntro.java
+++ src/day_22_arrayList/WrapperClassIntro.java
@@ -1,2 +1,35 @@
-package day_22_arrayList;public class WrapperClassIntro {
+package day_22_arrayList;
+
+public class WrapperClassIntro {
+    public static void main(String[] args) {
+        String str = "java";
+
+        int a = 10;
+        Integer b = 10;
+
+
+        System.out.println("-----------------------------\n");
+
+        int c1 = 100;
+        Integer c2 = c1;//autoboxing primitive to wrapper: Wrapper clas accept only one variable type.
+
+        char ch1 = 'A';
+        Character ch2 = ch1;
+
+        double db1 = 2.3;
+        Double db2 = db1;
+        System.out.println("------------------------------------------\n");
+
+        Integer n1 = 10;
+
+        int n2 = n1; // unboxing
+
+        Character e1 = 'A';
+        char e2 = e1;
+
+        System.out.println("----------------------------------\n");
+
+
+
+    }
 }
