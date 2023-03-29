e6e185d7b2a5c12b5719627f660794813a4caee2
Ismailduman <issmailduman@gmail.com>
2023-03-29 18:45:28 +0200

    max min of multiple dimension array

---
+++ src/practiseTasks/Fragment.java
@@ -0,0 +1,18 @@
+package practiseTasks;
+
+public class Fragment {
+    public static void doSum(){
+
+
+    }
+    public static void main(String[] args) {
+//        int [] arr= {1,2,3,4};
+//        int i=0;
+//        do{
+//            System.out.println(arr[i]+" ");
+//            i++;
+//        }while(i<arr.length+1);
+
+
+    }
+}
