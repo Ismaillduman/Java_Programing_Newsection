6e5c2c15433ff15ee21134e76d3e4106523eef71
Ismailduman <issmailduman@gmail.com>
2023-04-02 17:22:25 +0200

    average of each array

--- src/reviews/review_08/AverageOfNumber.java
+++ src/reviews/review_08/AverageOfNumber.java
@@ -5,22 +5,26 @@ import java.util.Arrays;
 
 public class AverageOfNumber {
     public static void main(String[] args) {
-        int[][] nums={
-                {1,2,3,4,5},
-                {10,20,330,54,7,8},
-                {89,78,45,12}
+        int[][] nums = {
+                {1, 2, 3, 4, 5},
+                {10, 20, 330, 54, 7, 8},
+                {89, 78, 45, 12}
         };
-        System.out.println(Arrays.deepToString(nums));
-int sumEach=0;
-        for(int[] d1nums:nums){
-            System.out.println(Arrays.toString(d1nums));
-           for(int each:d1nums){
-               System.out.println(each);
-               sumEach+=each;
-           }
+
+        int sumEach = 0;
+        for (int[] d1nums : nums) {
+            int sumOfEachArr=0;
+            for (int each : d1nums) {
+
+                sumOfEachArr+=each;
+            }
+            double averageOfEachArr=sumOfEachArr/(double)d1nums.length;
+            sumEach+=sumOfEachArr;
+            System.out.println("average of: "+Arrays.toString(d1nums)+" "+averageOfEachArr);
         }
-        DecimalFormat df= new DecimalFormat("0.00");
-        double average= sumEach/(double)nums.length;
-        System.out.println(df.format(average));
+        DecimalFormat df = new DecimalFormat("0.00");
+        System.out.println("Total Average of numbers: "+df.format((double)sumEach/nums.length));
+
+
     }
 }
