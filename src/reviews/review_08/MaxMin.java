dd5cfea82e786d67b6cb82dd38009421aacc5712
Ismailduman <issmailduman@gmail.com>
2023-04-02 22:04:21 +0200

    new approach

--- src/reviews/review_08/MaxMin.java
+++ src/reviews/review_08/MaxMin.java
@@ -6,13 +6,28 @@ import java.util.Collections;
 
 public class MaxMin {
     public static void main(String[] args) {
-        ArrayList<Integer> nums= new ArrayList<>(Arrays.asList(1,2,3,4,56,78,9,899,8));
+        ArrayList<Integer> nums= new ArrayList<>(Arrays.asList(4,8,7,8,8));
 
-        int max= Collections.max(nums);
-        System.out.println("first max: "+max);
-        nums.removeAll(Arrays.asList(max));
-        max= Collections.max(nums);
-        System.out.println("second max: "+max);
+//        int max= Collections.max(nums);
+//        System.out.println("first max: "+max);
+//        nums.removeAll(Arrays.asList(max));
+//        max= Collections.max(nums);
+//        System.out.println("second max: "+max);
+
+        int max = nums.get(0);
+        int secondMax= nums.get(0);
+        for(int each:nums){
+            if(each>max){
+                secondMax=max;
+                max=each;
+            }
+            if(each>secondMax&&each<max){
+                secondMax=each;
+            }
+        }
+        System.out.println("secondMax = " + secondMax);
+        System.out.println("max = " + max);
+
 
     }
 }
