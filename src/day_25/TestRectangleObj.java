d291207a2e9fabd96c03b194fe1b8257b3d3374c
Ismailduman <issmailduman@gmail.com>
2023-04-06 02:03:44 +0200

    constructor with paremeter

---
+++ src/day_25/TestRectangleObj.java
@@ -0,0 +1,19 @@
+package day_25;
+
+public class TestRectangleObj {
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle(5,4);
+        Rectangle rectangle2 = new Rectangle(8,6);
+        Rectangle rectangle3 = new Rectangle(12,18);
+//
+//        rectangle1.setInfo(5, 4);
+//        rectangle2.setInfo(9, 8);
+//        rectangle3.setInfo(7, 3);
+
+        System.out.println(rectangle1);
+        System.out.println(rectangle2);
+        System.out.println(rectangle3);
+
+
+    }
+}
