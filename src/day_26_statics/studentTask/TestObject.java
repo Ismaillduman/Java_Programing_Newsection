eb02ac57cdd2957327a2313cb2027a2620a4c299
Ismailduman <issmailduman@gmail.com>
2023-04-08 00:36:50 +0200

    test object

---
+++ src/day_26_statics/studentTask/TestObject.java
@@ -0,0 +1,11 @@
+package day_26_statics.studentTask;
+
+public class TestObject {
+    public static void main(String[] args) {
+        Student student1= new Student("Ismail","12345asd",39,'M');
+        Student student2= new Student("Busra","123d",10,'F');
+        Student student3= new Student("Yusuf","2sd",8,'M');
+        Student student4= new Student("Zübeyde","14asd",41,'F');
+        Student student5= new Student("Damla","345as",3,'F');
+    }
+}
