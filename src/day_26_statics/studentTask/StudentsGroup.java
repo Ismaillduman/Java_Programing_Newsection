9ffdef87bfa7eadb087877f4b13bbced909b81d4
Ismailduman <issmailduman@gmail.com>
2023-04-07 17:35:37 +0200

    Student

---
+++ src/day_26_statics/studentTask/StudentsGroup.java
@@ -0,0 +1,10 @@
+package day_26_statics.studentTask;
+
+public class StudentsGroup {
+    public static void main(String[] args) {
+        Student student=new Student("Büsra","123456",10,'F');
+        System.out.println(student);
+        student.study();
+    }
+
+}
