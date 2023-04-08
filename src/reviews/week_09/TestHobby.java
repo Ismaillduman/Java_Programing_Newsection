e97a47179bbe065bb249f05ee9845cfd6a7389d7
Ismailduman <issmailduman@gmail.com>
2023-04-08 18:43:55 +0200

    hobby test object manipulation

--- src/reviews/week_09/TestHobby.java
+++ src/reviews/week_09/TestHobby.java
@@ -6,26 +6,37 @@ import java.util.Arrays;
 
 public class TestHobby {
     public static void main(String[] args) {
-        Hobby golf= new Hobby("Golf",25,true,false);
-        Hobby puzzle= new Hobby("Puzzle",12,false,false);
-        Hobby java= new Hobby("java");
-        Hobby dancing= new Hobby("Salsa",45,false,true);
-        Hobby bowling= new Hobby("Bowling",50,false,true);
-        Hobby tennis= new Hobby("Tennis",60,true,true);
+        Hobby golf = new Hobby("Golf", 2500, true, false);
+        Hobby puzzle = new Hobby("Puzzle", 120, false, false);
+        Hobby java = new Hobby("java");
+        Hobby dancing = new Hobby("Salsa", 450, false, true);
+        Hobby bowling = new Hobby("Bowling", 500, false, true);
+        Hobby tennis = new Hobby("Tennis", 600, true, true);
 
-        ArrayList<Hobby> hobbies= new ArrayList<>();
-        hobbies.addAll(Arrays.asList(golf,puzzle,java,dancing,bowling,tennis));
+        ArrayList<Hobby> hobbies = new ArrayList<>();
+        hobbies.addAll(Arrays.asList(golf, puzzle, java, dancing, bowling, tennis));
 
-        hobbies.addAll(Arrays.asList(new Hobby("ping pin",200,false,true),
-                new Hobby("volleyball",100,false,true),
-                new Hobby("Swimming",150,true,false)));
+        hobbies.addAll(Arrays.asList(new Hobby("ping pin", 200, false, true),
+                new Hobby("volleyball", 100, false, true),
+                new Hobby("Swimming", 150, true, false)));
 
         System.out.println(hobbies);
 
         System.out.println("-----------------------------------------------");
-        for(Hobby each:hobbies){
+        for (Hobby each : hobbies) {
             each.doIt();
         }
+        System.out.println("--------------------------------------------");
+        ArrayList<Hobby> outdoors = new ArrayList<>(hobbies);
+        ArrayList<Hobby> requireOthers = new ArrayList<>(hobbies);
+        ArrayList<Hobby> highCost = new ArrayList<>(hobbies);
+        outdoors.removeIf(p-> !p.isOutdoors);
+        requireOthers.removeIf(p->p.requiresOthers);
+        highCost.removeIf(p->p.annualCost>400);
+        System.out.println(outdoors);
+        System.out.println(requireOthers);
+        System.out.println(highCost);
+
     }
 
 }
