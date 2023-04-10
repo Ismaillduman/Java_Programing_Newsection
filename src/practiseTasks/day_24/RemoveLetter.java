6496f81196fcd68ce6a4c39a8f70e319b8da5dee
Ismailduman <issmailduman@gmail.com>
2023-04-10 13:44:46 +0200

    static import all

--- src/practiseTasks/day_24/RemoveLetter.java
+++ src/practiseTasks/day_24/RemoveLetter.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 public class RemoveLetter {
     public static void main(String[] args) {
         ArrayList<Character> list= new ArrayList<>(Arrays.asList('$', 'A', 'B', '1', '2', 'C', 'D', '@', '!', '3', '4'));
-        list.removeIf(p->Character.isLetter(p));
+        list.removeIf(p->!Character.isLetter(p));
         System.out.println(list);
 
         System.out.println("------------------------------");
         ArrayList<Character> characters= new ArrayList<>(Arrays.asList('$', 'A', 'B', '1', '2', 'C', 'D', '@', '!', '3', '4'));
-        characters.removeIf(p->Character.isLetterOrDigit(p));
+        characters.removeIf(Character::isLetterOrDigit);
         System.out.println(characters);
     }
 }
--- src/utilities/Test2.java
+++ src/utilities/Test2.java
@@ -1,7 +1,16 @@
 package utilities;
 
+//import day_27_access_modifiers.Data;
+import static day_27_access_modifiers.Data.d;
+import static day_27_access_modifiers.Data.method3;
+import static day_27_access_modifiers.Data.*;//all static members with star *
 public class Test2 {
     public static void main(String[] args) {
-
+       // System.out.println(Data.d);
+        System.out.println(d);
+        method3();
+        System.out.println(e);
+        System.out.println(f);
+        method4();
     }
 }
