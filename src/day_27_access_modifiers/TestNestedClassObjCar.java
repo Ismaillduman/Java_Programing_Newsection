071484fbe5340f8e0803607a5c435eda18ac3cbe
Ismailduman <issmailduman@gmail.com>
2023-04-10 15:34:04 +0200

    nested class

---
+++ src/day_27_access_modifiers/TestNestedClassObjCar.java
@@ -0,0 +1,13 @@
+package day_27_access_modifiers;
+
+public class TestNestedClassObjCar {
+
+    public static void main(String[] args) {
+        Car obj1= new Car(); //car object
+        Car.CarEngine obj2= obj1.new CarEngine();//carEngine object
+
+Car.StaticInnerClass obj3= new Car.StaticInnerClass();
+
+
+    }
+}
