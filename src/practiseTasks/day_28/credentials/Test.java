3505369156750ea3555f364e8a2aa6c4413df344
Ismailduman <issmailduman@gmail.com>
2023-04-12 01:33:03 +0200

    credentials

---
+++ src/practiseTasks/day_28/credentials/Test.java
@@ -0,0 +1,8 @@
+package practiseTasks.day_28.credentials;
+
+public class Test {
+    public static void main(String[] args) {
+        Credentials credentials= new Credentials("Ismail","123a# 456");
+        System.out.println(credentials);
+    }
+}
