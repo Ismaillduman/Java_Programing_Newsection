2a13290738b26a70f325c6ee497361838231ba9a
Ismailduman <issmailduman@gmail.com>
2023-04-11 17:49:44 +0200

    Employee

--- src/day_28_encapsulation/encapsulation/TestEmployee.java
+++ src/day_28_encapsulation/encapsulation/TestEmployee.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 
 public class TestEmployee {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        Employee employee= new Employee();
-        employee.setSalary(sc.nextDouble());
-        System.out.println(employee.getSalary());
+//        System.out.println("Enter salary");
+//        Scanner sc = new Scanner(System.in);
+//
+//        Employee employee = new Employee();
+//
+//        employee.setSalary(sc.nextDouble());
+//        System.out.println(employee.getSalary());
+//
+//        //employee.setName(" ");
+//        System.out.println(employee.getName());
+        Employee employee1=new Employee("ismail",39,"SDET",55000);
+        System.out.println(employee1);
     }
 }
