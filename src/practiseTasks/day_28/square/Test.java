9328d728eed3bd0caa5c62c924a12bcdb5835a01
Ismailduman <issmailduman@gmail.com>
2023-04-11 19:03:22 +0200

    square  encapsulation test

---
+++ src/practiseTasks/day_28/square/Test.java
@@ -0,0 +1,11 @@
+package practiseTasks.day_28.square;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        Square square= new Square(4);
+        System.out.println(square);
+        System.out.println(square.getSide());
+    }
+}
