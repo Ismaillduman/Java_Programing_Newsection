dda9de81af34864fcfdaf15cde842c6f16c989bb
Ismailduman <issmailduman@gmail.com>
2023-04-12 23:12:21 +0200

    credentials test

---
+++ src/trySome.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class trySome {
+    public static void main(String[] args) {
+        String password ="password";
+        String[] passwordArray= password.split("");
+        System.out.println(Arrays.toString(passwordArray));
+    }
+}
