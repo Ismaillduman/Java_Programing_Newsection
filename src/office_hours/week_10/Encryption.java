fbc47927ff4822418bd973424afbaded68c67fb5
Ismailduman <issmailduman@gmail.com>
2023-04-13 01:26:43 +0200

    ecryption test

--- src/office_hours/week_10/Encryption.java
+++ src/office_hours/week_10/Encryption.java
@@ -26,33 +26,20 @@ public class Encryption {
         message=encrypted.trim();
     }
     public static String decrypt(String message,int modifyNumber,int factorNum ){
-        String[] eachPart=message.split("");
+        String[] eachPart=message.split(" ");
         String decrypted="";
         for (String each:eachPart) {
-            decrypted+=(char)(Integer.parseInt(each)/factorNum)-modifyNumber+" ";
+            decrypted+=(char)(Integer.parseInt(each)/factorNum-modifyNumber);
+
         }
         return decrypted;
     }
 
-    @Override
+
     public String toString() {
         return "Encryption{" +
                 "message='" + message + '\'' +
                 '}';
     }
-/*
 
-    create a method decrypt (static)
-
-
-        Given the modify and factor numbers decrypt the message and return the decrypted String.
-         Do the reverse steps we did for encrypting
-
-        Ex:
-        message: 252 234 258 242 270 104 236 262 260 240
-        modify num: 20
-        factor num: 2
-
-        -> james bond
-*/
 }
