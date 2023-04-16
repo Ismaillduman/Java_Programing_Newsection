c9b808550fc15a315bff648ba3596460c3eb0513
Ismailduman <issmailduman@gmail.com>
2023-04-16 16:24:23 +0200

    auidi bmw mercedes volvo and my car

---
+++ src/practiseTasks/day_29/cars/Mercedes.java
@@ -0,0 +1,4 @@
+package practiseTasks.day_29.cars;
+
+public class Mercedes extends Car {
+}
---
+++ src/practiseTasks/day_29/cars/MyCar.java
@@ -0,0 +1,13 @@
+package practiseTasks.day_29.cars;
+
+public class MyCar {
+
+
+    public static void main(String[] args) {
+        Volvo volvo= new Volvo();
+        volvo.setInfo("Volvo","c90","Silver",2022,40000);
+        System.out.println(volvo);
+    }
+
+
+}
