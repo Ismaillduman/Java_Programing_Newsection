546800c11672f417cc15bdba5bbdb46a656ba7f8
Ismailduman <issmailduman@gmail.com>
2023-04-16 16:11:36 +0200

    car

---
+++ src/practiseTasks/day_29/cars/Car.java
@@ -0,0 +1,66 @@
+package practiseTasks.day_29.cars;
+
+public class Car {
+    private String make, model,color;
+    private int year;
+    private double price;
+
+    public void setInfo(String make, String model, String color, int year, double price) {
+       setMake(make);
+       setColor(color);
+       setModel(model);
+       setPrice(price);
+       setYear(year);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() +
+                " make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                '}';
+    }
+}
