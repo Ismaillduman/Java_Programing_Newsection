99d44a9397171368881992657fe619edb8921da9
Ismailduman <issmailduman@gmail.com>
2023-04-20 00:54:37 +0200

    toyota audi yatch

---
+++ src/daily_lessons/day_31/vehicle/Audi.java
@@ -0,0 +1,8 @@
+package daily_lessons.day_31.vehicle;
+
+public class Audi extends Car{
+    public Audi(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+
+}
---
+++ src/daily_lessons/day_31/vehicle/Yacht.java
@@ -0,0 +1,7 @@
+package daily_lessons.day_31.vehicle;
+
+public class Yacht extends Boat{
+    public Yacht(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+}
