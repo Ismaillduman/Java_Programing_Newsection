8e4e29cdf2dc5553843e5bf0f02cce2234687881
Ismailduman <issmailduman@gmail.com>
2023-04-21 00:45:06 +0200

    virginia teas

---
+++ src/practiseTasks/day_31/states/Texas.java
@@ -0,0 +1,7 @@
+package practiseTasks.day_31.states;
+
+public class Texas extends States{
+    public Texas(String name, String politicalParty, String senator, String governor, String abbreviation, int population, double stateTask) {
+        super(name, politicalParty, senator, governor, "TX", population, stateTask);
+    }
+}
