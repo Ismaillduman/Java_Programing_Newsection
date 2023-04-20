ac226f01c4250b7a45d899f874fe232cea7b6697
Ismailduman <issmailduman@gmail.com>
2023-04-21 00:59:06 +0200

    Fire fox

---
+++ src/practiseTasks/day_31/browser/FireFox.java
@@ -0,0 +1,13 @@
+package practiseTasks.day_31.browser;
+
+public class FireFox extends Browser{
+    @Override
+    public void openBrowser() {
+        System.out.println("opening firefox browser");
+    }
+
+    @Override
+    public void closeBrowser() {
+        System.out.println("closing firefox browser");
+    }
+}
