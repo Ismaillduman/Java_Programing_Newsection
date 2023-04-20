9e6260704a72dbcd0fa84023a35176eceabd04d8
Ismailduman <issmailduman@gmail.com>
2023-04-21 00:48:54 +0200

    test

---
+++ src/practiseTasks/day_31/states/Florida.java
@@ -0,0 +1,8 @@
+package practiseTasks.day_31.states;
+
+public class Florida extends States{
+
+    public Florida(String name, String politicalParty, String senator, String governor, String abbreviation, int population, double stateTask) {
+        super(name, politicalParty, senator, governor, "FL", population, stateTask);
+    }
+}
