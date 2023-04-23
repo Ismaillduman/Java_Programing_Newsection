ba0e33ed72f8d94a06571e15a3ec9e927c37fa56
Ismailduman <issmailduman@gmail.com>
2023-04-23 23:30:50 +0200

    try some

---
+++ src/trySome.java
@@ -0,0 +1,12 @@
+public class trySome {
+     static int score;
+    static {
+        score=75;
+    }
+    public static void main(String[] args) {
+        score=5;
+        System.out.println(score);
+    }
+
+}
+
