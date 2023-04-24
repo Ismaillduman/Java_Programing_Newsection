dba31f07acadb053cd5925e2c41ba2c4ef10c4c2
Ismailduman <issmailduman@gmail.com>
2023-04-24 14:50:32 +0200

    toyota

---
+++ src/daily_lessons/day_32_final_keyword/car/Toyota.java
@@ -0,0 +1,9 @@
+package daily_lessons.day_32_final_keyword.car;
+
+public class Toyota extends Car{
+    public Toyota(String model, String color, int year, double price) {
+        super(model, color, year, price);
+    }
+
+
+}
