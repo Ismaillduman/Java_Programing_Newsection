95faccabc2d0e48932c36fd430d1008bcf7daab3
Ismailduman <issmailduman@gmail.com>
2023-04-24 13:17:27 +0200

    restaurant

--- src/office_hours/week_11/foodCourse/Restaurant.java
+++ src/office_hours/week_11/foodCourse/Restaurant.java
@@ -1,2 +1,8 @@
-package office_hours.week_11.foodCourse;public class Restaurant {
+package office_hours.week_11.foodCourse;
+
+public class Restaurant {
+    public static void main(String[] args) {
+        Fish fish= new Fish("Kevin",1.4,"lentil","anchovy");
+
+    }
 }
