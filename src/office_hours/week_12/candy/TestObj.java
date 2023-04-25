9f91e685fe8d881b1573215a042263543c85c05e
Ismailduman <issmailduman@gmail.com>
2023-04-25 13:03:42 +0200

    candy

---
+++ src/office_hours/week_12/candy/TestObj.java
@@ -0,0 +1,10 @@
+package office_hours.week_12.candy;
+
+public class TestObj {
+    public static void main(String[] args) {
+        Take5 take5= new Take5("haribo",5,false);
+        Twix twix= new Twix("Caramel",8,true);
+        System.out.println(twix);
+        System.out.println(take5);
+    }
+}
