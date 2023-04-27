c6fce1ebf698acf952adce87f4145368544d9bdb
Ismailduman <issmailduman@gmail.com>
2023-04-27 13:46:22 +0200

    some questions

---
+++ src/practiseTasks/outOfConcept/Output.java
@@ -0,0 +1,13 @@
+package practiseTasks.outOfConcept;
+
+public class Output {
+    public static void main(String[] args) {
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 3; j >=0 ; j--) {
+                if(i==j)
+                continue;
+                System.out.print(i+" "+j);
+            }
+        }
+    }
+}
