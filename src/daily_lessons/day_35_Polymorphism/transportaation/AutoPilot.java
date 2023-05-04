3ced5dab3496868770c27afc4259d0e8dd5bc56b
Ismailduman <issmailduman@gmail.com>
2023-05-04 17:37:00 +0200

    transportation interface

---
+++ src/daily_lessons/day_35_Polymorphism/transportaation/AutoPilot.java
@@ -0,0 +1,6 @@
+package daily_lessons.day_35_Polymorphism.transportaation;
+
+public interface AutoPilot extends AutoPark{
+    boolean hasAutoPilot =true;
+    void selfDrive();
+}
---
+++ src/daily_lessons/day_35_Polymorphism/transportaation/Electric.java
@@ -0,0 +1,5 @@
+package daily_lessons.day_35_Polymorphism.transportaation;
+
+public interface Electric {
+    void charge();
+}
