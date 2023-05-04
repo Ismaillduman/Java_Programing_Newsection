6811a905819ac9c960afb12066093a43e64dc5d2
Ismailduman <issmailduman@gmail.com>
2023-05-04 10:05:49 +0200

    Car with abstract and interface

---
+++ src/practiseTasks/day_34/car/AutoPark.java
@@ -0,0 +1,13 @@
+package practiseTasks.day_34.car;
+
+public interface AutoPark {
+    boolean hasAutoPark=true;
+
+    void autoPark();
+    /*2. Create an interface named AutoPark
+				Variables:
+					hasAutoPark
+
+				Abstract method:
+					autoPark();*/
+}
