bfb484b9975b87ec076c5ddb1ebde16b14fdaae7
Ismailduman <issmailduman@gmail.com>
2023-05-07 18:42:41 +0200

    Exercise test Bench

--- src/reviews/week_13/Gym.java
+++ src/reviews/week_13/Gym.java
@@ -7,6 +7,13 @@ public class Gym {
         Running running= new Running();
         System.out.println(running.getCaloriesBurned(60));
         running.perform();
+
+       //  Lifting lifting= new Lifting(); can not be able to create a obj from abstract class
+        System.out.println("========================================");
+        Bench bench= new Bench();
+        System.out.println(bench.getCaloriesBurned(60));
+        bench.rackWeight();
+        bench.perform();
     }
 
 }
