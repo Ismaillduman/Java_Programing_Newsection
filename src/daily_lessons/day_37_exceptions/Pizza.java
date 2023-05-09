94a01a3bd8f0332f00c29fc4842a9847090872a4
Ismailduman <issmailduman@gmail.com>
2023-05-09 10:26:04 +0200

    day_37 Pizza

---
+++ src/daily_lessons/day_37_exceptions/Pizza.java
@@ -0,0 +1,45 @@
+package daily_lessons.day_37_exceptions;
+
+public class Pizza {
+    private  char size;
+    private int numberOfCheeseTopping,numberOfPepperoniTopping;
+
+    public Pizza(char size, int numberOfCheeseTopping, int numberOfPepperoniTopping) {
+        this.size = size;
+        this.numberOfCheeseTopping = numberOfCheeseTopping;
+        this.numberOfPepperoniTopping = numberOfPepperoniTopping;
+    }
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setSize(char size) {
+        this.size = size;
+    }
+
+    public int getNumberOfCheeseTopping() {
+        return numberOfCheeseTopping;
+    }
+
+    public void setNumberOfCheeseTopping(int numberOfCheeseTopping) {
+        this.numberOfCheeseTopping = numberOfCheeseTopping;
+    }
+
+    public int getNumberOfPepperoniTopping() {
+        return numberOfPepperoniTopping;
+    }
+
+    public void setNumberOfPepperoniTopping(int numberOfPepperoniTopping) {
+        this.numberOfPepperoniTopping = numberOfPepperoniTopping;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "size=" + size +
+                ", numberOfCheeseTopping=" + numberOfCheeseTopping +
+                ", numberOfPepperoniTopping=" + numberOfPepperoniTopping +
+                '}';
+    }
+}
--- src/reviews/week_13/building/BuildingUtil.java
+++ src/reviews/week_13/building/BuildingUtil.java
@@ -9,4 +9,14 @@ if(building instanceof House){
 }
 return -1; //if not any of them
     }
+
+    public static Building search(String location){
+        if(location.equals("New York")){
+            return new House("New York",4500000,5);
+        } else if (location.equals("Köln")) {
+            return new House("Köln",100000,6);
+        }else{
+            return null;
+        }
+    }
 }
