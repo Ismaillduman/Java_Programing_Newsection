d091ee7bf0339ccf1d469304838e33181ccc6cc0
Ismailduman <issmailduman@gmail.com>
2023-05-10 00:29:36 +0200

    multiple catch block

--- src/daily_lessons/day_37_exceptions/MultipleCatchBlock.java
+++ src/daily_lessons/day_37_exceptions/MultipleCatchBlock.java
@@ -1,7 +1,31 @@
 package daily_lessons.day_37_exceptions;
 
+import daily_lessons.day_35_Polymorphism.transportaation.Car;
+
 public class MultipleCatchBlock {
     public static void main(String[] args) {
-
+        System.out.println("Program1 started");
+        Car car=null;
+        try {
+            car.drive();
+        }catch (ArithmeticException e){
+            System.out.println("First Catch Block");
+            e.printStackTrace();
+        }catch (ClassCastException e){
+            System.out.println("Second Catch Block");
+            e.printStackTrace();
+        }catch (NullPointerException e){
+            System.out.println("Third Catch Block");
+            e.printStackTrace();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Fourth Catch Block");
+            e.printStackTrace();
+        }catch (RuntimeException e){
+            System.out.println("Fifth Catch Block");
+            e.printStackTrace();
+        }
+        System.out.println("Program Ended");
+        }
+        //every try block muss have  at least one catch block. one try block can have multiple catch block
     }
-}
+
