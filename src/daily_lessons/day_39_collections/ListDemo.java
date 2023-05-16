aac607c42ee7c4adb5347f10f77a7478948c8c6d
Ismailduman <issmailduman@gmail.com>
2023-05-16 13:39:09 +0200

    ListDemo

---
+++ src/daily_lessons/day_39_collections/ListDemo.java
@@ -0,0 +1,36 @@
+package daily_lessons.day_39_collections;
+
+import practiseTasks.day_31.states.States;
+
+import java.util.*;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(100, 200, 300));
+
+        arrayList.remove(0);
+        arrayList.get(0);
+        System.out.println(arrayList);
+        System.out.println("---------------------------------");
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.addAll(Arrays.asList(100, 200, 300));
+        linkedList.remove(0);
+        linkedList.get(0);
+        System.out.println(linkedList);
+        System.out.println("---------------------------------------------");
+        Vector<Integer> vector = new Vector<>(Arrays.asList(100, 200, 300));
+        vector.remove(0);
+        vector.get(0);
+        System.out.println(vector);
+        System.out.println("-------------------------------------");
+        Stack<Integer> stack = new Stack<>();
+        stack.addAll(Arrays.asList(100, 200, 300));
+        System.out.println(stack);
+        int lastElement = stack.pop();
+        System.out.println(stack);
+        int secondLastElement = stack.peek();
+        System.out.println(secondLastElement);
+
+    }
+}
