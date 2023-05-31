527c4fb861a3d4dd7be7373a6c40d3b1a514837e
Ismailduman <issmailduman@gmail.com>
2023-05-31 23:40:03 +0200

    mapAB3

--- src/office_hours/week_17/map_1/MapAB3.java
+++ src/office_hours/week_17/map_1/MapAB3.java
@@ -1,5 +1,15 @@
 package office_hours.week_17.map_1;
 
-public class MapAB3 {
+import java.util.Map;
 
+public class MapAB3 {
+public Map<String,String> map(Map<String,String> map){
+
+    if(map.containsKey("a")&&!map.containsKey("b")){
+        map.put("b",map.get("a"));
+    } else if (map.containsKey("b")&&!map.containsKey("a")) {
+        map.put("a",map.get("b"));
+    }
+    return map;
+}
 }
